d50a01c24949ef7c87f1bad2848b75a4f171646b
Annie Kovač <anniekovac95@gmail.com>
2017-10-11 21:45:37 +0200

    Added main file
    For now, reading input line is implemented.

---
+++ fuzzy.py
@@ -0,0 +1,16 @@
+class UniversalSet(object):
+    def __init__():
+        value = None
+
+
+class FuzzySet(UniversalSet):
+    pass
+
+if __name__ == "__main__":
+
+    # getting input from keyboard and turning it to integers
+    my_input = input("Please enter L D LK DK V S in this order:")
+    nums_from_input = [int(char) for char in my_input if char.isdigit()]
+    if len(nums_from_input) != 6:
+        raise ValueError("Your input is invalid!")
+    L, D, LK, DK, V, S = nums_from_input
