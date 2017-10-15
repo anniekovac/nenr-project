47b32d12edc7dcc845e6ecf60d9d7562544b6a97
Annie Kovač <anniekovac95@gmail.com>
2017-10-15 18:20:10 +0200

    Added functionalities to domain.py
    Added new methods and functionalities

--- first/domain.py
+++ first/domain.py
@@ -1,51 +1,23 @@
-class DomainElement(object):
+import itertools
 
-    def get_number_of_components(self):
-        """
-        :return: int 
-        """
-        pass
-
-    def get_component_value(self, component):
-        """
-        :param component: int 
-        :return: int
-        """
-        pass
-
-    def hash_code(self):
-        """
-        :return: int
-        """
-        pass
-
-    def equals(self, instance):
-        """
-        :param instance: Object 
-        :return: boolean
-        """
-        pass
-
-    def to_string(self):
-        """
-        :return: string 
-        """
-        pass
 
 class Domain(object):
+
+    domain_elements = []
+
     def index_of_element(self, domain_element):
         """
         :param domain_element: DomainElement 
         :return: int
         """
-        pass
+        return self.domain_elements.index(domain_element)
 
     def element_for_index(self, index):
         """
         :param index: int 
         :return: DomainElement
         """
-        pass
+        return self.domain_elemens[index]
 
 
 class SimpleDomain(Domain):
@@ -59,21 +31,22 @@ class SimpleDomain(Domain):
         self.first = first
         self.last = last
         self.domain_name = domain_name
+        self.domain_elements = [element for element in range(self.first, self.last)]
 
     def print_simple_domain(self):
         """
         Printing simple domain. 
         """
         print("Elementi domene {}:".format(self.domain_name))
-        for element in range(self.first, self.last):
+        for element in self.domain_elements:
             print("Element domene: {}".format(element))
-        print("Kardinalitet domene je: ".format(self.get_cardinality()))
+        print("Kardinalitet domene je: {}".format(self.get_cardinality()))
 
     def get_cardinality(self):
         """
         :return: int 
         """
-        return len([element for element in range(self.first, self.last)])
+        return len(self.domain_elements)
 
     def get_number_of_components(self):
         """
@@ -105,15 +78,41 @@ class SimpleDomain(Domain):
         return self.last
 
 class CompositeDomain(Domain):
+    """
+    This class represents Domain consisted of multiple
+    SimpleDomains. This means that its elements are Cartesian
+    product of elements from every SimpleDomain given to it.
+    """
 
-    def __init__(self, list_of_domains):
+    def __init__(self, list_of_domains, domain_name=None):
         self.list_of_domains = list_of_domains
+        self.domain_elements = []
+        self.domain_name = domain_name
+
+        # creating list of lists
+        # in these lists there are elements of every specific
+        # SimpleDomain in order to create cartesian product
+        list_for_cartesian = [element.domain_elements for element in self.list_of_domains]
+
+        # creating cartesian product and appending these elements
+        # to element list
+        for element in itertools.product(*list_for_cartesian):
+            self.domain_elements.append(element)
+
+    def print_composite_domain(self):
+        """
+        Printing CompositeDomain. 
+        """
+        print("Elementi domene {}:".format(self.domain_name))
+        for element in self.domain_elements:
+            print("Element domene: {}".format(element))
+        print("Kardinalitet domene je: {}".format(self.get_cardinality()))
 
     def get_cardinality(self):
         """
         :return: int 
         """
-        pass
+        return len(self.domain_elements)
 
     def get_number_of_components(self):
         """
@@ -133,6 +132,9 @@ class CompositeDomain(Domain):
         return self.list_of_domains[index]
 
 if __name__ == "__main__":
-    simple_domain = SimpleDomain(1, 20)
+    simple_domain = SimpleDomain(1, 3, "Pero")
+    simple_domain2 = SimpleDomain(0, 4, "Jurica")
     simple_domain.print_simple_domain()
-
+    simple_domain2.print_simple_domain()
+    composite_domain = CompositeDomain([simple_domain, simple_domain2], "Barbara")
+    composite_domain.print_composite_domain()
