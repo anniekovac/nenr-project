19a5a1004d24102153414c77faa0e9fdbca93397
Annie Kovač <anniekovac95@gmail.com>
2017-10-16 09:48:27 +0200

    Added function unitary_function
    Beginning of functionalities for unitary function. For now, only step function is implemented.

--- first/sets.py
+++ first/sets.py
@@ -31,6 +31,7 @@ class FuzzySet(object):
 		return self.memberships[index]
 
 
+
 class MutableFuzzySet(FuzzySet):
 	def __init__(self, domain, set_name):
 		self.domain = domain
@@ -47,6 +48,19 @@ class MutableFuzzySet(FuzzySet):
 		self.memberships[index] = element_value
 		return self
 
+def unitary_function(domain):
+	"""
+	Setting double values to a list based on
+	domain.domain_elements list.
+	:param domain: Domain
+	:return: list of doubles
+	"""
+	### STEP FUNCTION
+	# this is approximately the middle
+	middle = int(float(len(domain.domain_elements))/2)
+	output_list = [0 if index < middle else 1 for (index, element) in enumerate(domain.domain_elements)]
+	return output_list
+
 class CalculatedFuzzySet(FuzzySet):
 
 	def __init__(self, domain, unitary_function):
@@ -58,4 +72,5 @@ if __name__ == "__main__":
 	my_fuzzy = MutableFuzzySet(simple_domain, "PeroSet")
 	my_fuzzy.set_value_at(1, 0.4)
 	my_fuzzy.set_value_at(2, 0.9)
-	my_fuzzy.print_fuzzy_set()
+	#my_fuzzy.print_fuzzy_set()
+	unitary_function(simple_domain)
