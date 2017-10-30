28d05cc62a2c9379e8dc0795a11039efce935903
Annie Kovač <anniekovac95@gmail.com>
2017-10-30 16:45:45 +0100

    Added new python module for defining classes and functions related to fuzzy control of a boat

---
+++ first/fuzzy_control.py
@@ -0,0 +1,11 @@
+from sets import MutableFuzzySet, CalculatedFuzzySet
+from domain import SimpleDomain, CompositeDomain
+
+
+class Rule(CalculatedFuzzySet):
+	# ovdje definirati domenu
+	# osim domene definirati kojom se funkcijom izracunavaju
+	# vrijednosti fuzzy set-a
+	# prilagoditi CalculatedFuzzySet klasu
+	# da moze primati neku drugu funkciju osim onih prethodno definiranih
+	pass
