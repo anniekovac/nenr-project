9a16a9b64f148b0a77247cdbff29cab0a5c62c7e
Annie Kovač <anniekovac95@gmail.com>
2017-11-01 16:16:02 +0100

    Added function for plotting fuzzy set

--- first/fuzzy_control.py
+++ first/fuzzy_control.py
@@ -1,9 +1,21 @@
+from matplotlib import pyplot
 from sets import MutableFuzzySet, CalculatedFuzzySet
 from domain import SimpleDomain, CompositeDomain
 
 
+def plot_fuzzy_set(fuzzy_set, y_axis_title="Membership function", x_axis_title="Domain"):
+	yPoints = fuzzy_set.memberships
+	xPoints = fuzzy_set.domain.domain_elements
+	pyplot.stem(xPoints, yPoints)
+	pyplot.xlabel(x_axis_title, fontsize=18)
+	pyplot.ylabel(y_axis_title, fontsize=18)
+	pyplot.show()
+
+
 def fuzzification(singleton, first, last):
 	"""
+	This function is used for fuzzification of singleton value.
+	It creates fuzzy set out of singleton value.
 	:param singleton: int (single value of input variable) 
 	:param first: int (first element of domain)
 	:param last: int (last element of domain - not included in the domain) 
@@ -23,6 +35,8 @@ class Rule(CalculatedFuzzySet):
 if __name__ == "__main__":
 	# domena ovisi o tome je li pravilo za akceleraciju
 	# ili za kut
-	angle_domain = SimpleDomain(-90, 90)
+	angle_domain = SimpleDomain(-90, 91)
 	my_angle_rule = Rule(angle_domain)
-	my_angle_rule.print_fuzzy_set()
+	my_singleton = 2
+	my_fuzzyficated = fuzzification(my_singleton, 0, 5)
+	plot_fuzzy_set(my_fuzzyficated)
