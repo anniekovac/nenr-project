ff403a8358dd3240e93f826acb420ffebb3bcca2
Annie Kovač <anniekovac95@gmail.com>
2017-11-01 15:51:38 +0100

    Starting to work on third exercise

--- first/fuzzy_control.py
+++ first/fuzzy_control.py
@@ -3,9 +3,14 @@ from domain import SimpleDomain, CompositeDomain
 
 
 class Rule(CalculatedFuzzySet):
+
 	# ovdje definirati domenu
 	# osim domene definirati kojom se funkcijom izracunavaju
 	# vrijednosti fuzzy set-a
 	# prilagoditi CalculatedFuzzySet klasu
 	# da moze primati neku drugu funkciju osim onih prethodno definiranih
+
+	# domena ovisi o tome je li pravilo za akceleraciju
+	# ili za kut
+	domain = SimpleDomain(first, last)
 	pass
--- first/main.py
+++ first/main.py
@@ -1,9 +1,19 @@
 if __name__ == "__main__":
-	# getting input from keyboard and turning it to integers
-	my_input = input("Please enter L D LK DK V S in this order:")
-	nums_from_input = [int(char) for char in my_input if char.isdigit()]
 
-	if len(nums_from_input) != 6:
-		raise ValueError("Your input is invalid, you have to enter exactly 6 numbers!")
+	while True:
 
-	L, D, LK, DK, V, S = nums_from_input
+		# getting input from keyboard and turning it to integers
+		my_input = input("Please enter L D LK DK V S in this order:")
+		if "KRAJ" in my_input:
+			break
+		nums_from_input = [int(char) for char in my_input if char.isdigit()]
+		L, D, LK, DK, V, S = nums_from_input
+
+		# L - udaljenost broda od obale prema lijevo
+		# D - udaljenost broda od obale prema desno
+		# LK - udaljenost broda od obale naprijed lijevo pod 45
+		# DK - udaljenost broda od obale naprijed desno pod 45
+		# V - iznos brzine broda
+		# S - podatak o tome krece li se brod u pravom smjeru (1 ako da, 0 ako ne)
+
+
