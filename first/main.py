e93f4f653bf8e1c24a26c2b9202e8a4258621ed0
anniekovac <anniekovac95@gmail.com>
2017-11-10 18:06:16 +0100

    Added writing inputs to file for easier testing

--- first/main.py
+++ first/main.py
@@ -1,5 +1,6 @@
-from fuzzy_control import RuddRuleBase, AccRuleBase
-
+import sys
+from fuzzy_control import RuddRuleBase, AccRuleBase, defuzzyfication, plot_fuzzy_set
+from fuzzy_inputs import *
 
 if __name__ == "__main__":
 
@@ -15,9 +16,23 @@ if __name__ == "__main__":
 		L, D, LK, DK, V, S = nums_from_input
 		input_dict = dict(L=L, D=D, LK=LK, DK=DK, V=V, S=S)
 
+
 		# for the rudder
 		rudd.instant_values = input_dict
 		rudd.update_input_values_for_rules()
+		fuzzy_k = rudd.calculate_rule_union()
+		K = defuzzyfication(fuzzy_k)
 
+		my_val_string = str(nums_from_input).replace(",", "")
+		with open("original_input.txt", "a") as f:
+			f.write("\n{}, K={}".format(my_val_string, K))
+
+		# for the acceleration
 		acc.instant_values = input_dict
+		acc.update_input_values_for_rules()
+		fuzzy_a = acc.calculate_rule_union()
+		A = defuzzyfication(fuzzy_a)
+
+		print("{} {}".format(A, int(K)))
+		#sys.stdout.flush()
 
