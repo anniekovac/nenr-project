641119dce26a2899ea962962854cb78a172a7107
anniekovac <anniekovac95@gmail.com>
2017-11-12 14:33:44 +0100

    Added documentation and comments for repr_my_base.py.

--- first/repr_my_base.py
+++ first/repr_my_base.py
@@ -2,10 +2,16 @@ from fuzzy_control import RuddRuleBase, AccRuleBase, plot_fuzzy_set, defuzzyfica
 from fuzzy_inputs import *
 
 if __name__ == "__main__":
-
+	"""
+	This program is made for testing one of the
+	rule bases created in the exercise. It will plot
+	resultant FuzzySet and show defuzzyficated value of this
+	resultant FuzzySet.
+	"""
 
 	my_input = input("Please select one of my bases (type 'acc' or 'rudd'): ")
 
+	# defining which base we will be testing
 	if my_input == "rudd":
 		my_base = RuddRuleBase()
 	elif my_input == "acc":
@@ -13,11 +19,13 @@ if __name__ == "__main__":
 	else:
 		raise ValueError("None of the above is selected!")
 
+	# saving input variables to dictionary
 	my_input = input("Enter L, D, LK, DK, V, S:\n")
 	nums_from_input = [int(s) for s in my_input.split(" ") if s.isdigit()]
 	L, D, LK, DK, V, S = nums_from_input
 	input_dict = dict(L=L, D=D, LK=LK, DK=DK, V=V, S=S)
 
+	# fuzzy logic part
 	my_base.instant_values = input_dict
 	my_base.update_input_values_for_rules()
 	fuzzy_result = my_base.calculate_rule_union()
