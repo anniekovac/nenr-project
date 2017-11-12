2b5a2f73e1a6c2d5941b1e7cc7da307304e2f723
anniekovac <anniekovac95@gmail.com>
2017-11-12 14:36:11 +0100

    Added documentation and comments for repr_my_rule.py.

--- first/repr_my_rule.py
+++ first/repr_my_rule.py
@@ -2,10 +2,17 @@ from fuzzy_control import RuddRuleBase, AccRuleBase, plot_fuzzy_set, defuzzyfica
 from fuzzy_inputs import *
 
 if __name__ == "__main__":
-
-	#plot_fuzzy_set(my_acc_set)
+	"""
+	This program is made for testing one of the
+	rules created in the exercise. It will plot
+	resultant FuzzySet and show defuzzyficated value of this
+	resultant FuzzySet.
+	"""
+	# initializing both RuleBases
 	rudd = RuddRuleBase()
 	acc = AccRuleBase()
+
+	# getting input in which rule we will want to test
 	my_input = input("Please select one of my rules: ")
 	try:
 		my_rule = getattr(rudd, my_input)
@@ -16,15 +23,13 @@ if __name__ == "__main__":
 			print("Invalid choice! You can choose between: ")
 			raise
 
+	# saving input variables to dictionary
 	my_input = input("Enter L, D, LK, DK, V, S:\n")
 	nums_from_input = [int(s) for s in my_input.split(" ") if s.isdigit()]
 	L, D, LK, DK, V, S = nums_from_input
 	input_dict = dict(L=L, D=D, LK=LK, DK=DK, V=V, S=S)
 
 	my_rule.instant_values = input_dict
-	#import pdb; pdb.set_trace()
-	#plot_fuzzy_set(my_rule)
 	result = my_rule.calculate_fuzzy_rule()
-
 	plot_fuzzy_set(result)
-	print(defuzzyfication(result))
+	#print(defuzzyfication(result))
