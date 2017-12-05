23a9910c9df8c4b3bbaac5192d7dec4e1a92a7e2
anniekovac <anniekovac95@gmail.com>
2017-12-05 20:00:39 +0100

    Implementing discrete recombination of genes crossover

--- evolutionary_computing/main.py
+++ evolutionary_computing/main.py
@@ -1,12 +1,14 @@
 import numpy
 import os
 
+
 class Guess(object):
 	def __init__(self, gene):
 		"""
 		:param genes: list 
 		"""
 		self.gene = gene
+		self.fitness = None
 
 
 def my_function(gene, x, y):
@@ -58,27 +60,36 @@ def get_fitness(guess):
 	return fitness
 
 
-
 def crossover(guess1, guess2):
 	"""
-	:param guess1: 
-	:param guess2: 
-	:return: new guess 
+	This crossover implements discreete recombination
+	of genes.
+	:param guess1: Guess class instance 
+	:param guess2: Guess class instance
+	:return: Guess class instance
 	"""
-	pass
-
+	gene1 = guess1.gene
+	gene2 = guess2.gene
+	new_gene = numpy.array([numpy.random.choice([item1, item2]) for (item1, item2) in zip(gene1, gene2)])
+	new_gene_instance = Guess(new_gene)
+	return new_gene_instance
 
 def generate_population(n):
 	population = []
 	for guess in range(n):
 		genes = numpy.random.uniform(low=-4, high=4, size=(5,))
 		guess_instance = Guess(genes)
-		population.append(guess_instance)
 		fitness = get_fitness(guess_instance)
+		guess_instance.fitness = fitness
+		population.append(guess_instance)
 		print(fitness)
+	return population
+
 
 def main():
-	generate_population(5)
+	population = generate_population(5)
+	crossover(population[0], population[1])
+
 
 
 if __name__ == "__main__":
