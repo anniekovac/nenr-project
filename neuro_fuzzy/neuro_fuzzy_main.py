31f6d11a570500d64f058ff99fa35652da8696f3
anniekovac <anniekovac95@gmail.com>
2018-01-03 17:57:18 +0100

    added calculating of weights for input neurons,
    different implementation of input neurons - now they are organized in couples (see if thats ok?)

--- neuro_fuzzy/neuro_fuzzy_main.py
+++ neuro_fuzzy/neuro_fuzzy_main.py
@@ -26,6 +26,7 @@ class MembershipFunctionNeuron(InputNeuron):
 		self.membership_function = None
 		self.a = None
 		self.b = None
+		self.antecedent_type = None
 
 
 def set_net_acrhitecture(number_of_rules):
@@ -37,7 +38,7 @@ def set_net_acrhitecture(number_of_rules):
 							used in lists that represent layers
 	"""
 	# initializing number and types of neurons
-	input_neurons = numpy.array([MembershipFunctionNeuron(numpy.array([])) for i in range(2*number_of_rules)])
+	input_neurons = numpy.array([[MembershipFunctionNeuron(numpy.array([])), MembershipFunctionNeuron(numpy.array([]))] for i in range(number_of_rules)])
 	layer2 = numpy.array([Neuron(input_neurons) for i in range(number_of_rules)])
 	layer3 = numpy.array([Neuron(layer2) for i in range(number_of_rules)])
 	layer4 = numpy.array([Neuron(layer3) for i in range(number_of_rules)])
@@ -46,9 +47,17 @@ def set_net_acrhitecture(number_of_rules):
 
 	# initializing membership functions for neurons that have them
 	# neurons in input layer are using these membership functions
-	for neuron in input_neurons:
-		neuron.a = numpy.random.uniform(low=0, high=1) # a parameter of membership function
-		neuron.b = numpy.random.uniform(low=0, high=1) # b parameter of membership function
+	for index, neurons_couple in enumerate(input_neurons):
+
+		# first neuron - its fuzzy set is set A from antecedent
+		neural_net[0][index][0].a = numpy.random.uniform(low=0, high=1) # a parameter of membership function
+		neural_net[0][index][0].b = numpy.random.uniform(low=0, high=1) # b parameter of membership function
+		neural_net[0][index][0].antecedent_type = "A"
+
+		# second neuron - its fuzzy set is set B from antecedent
+		neural_net[0][index][1].a = numpy.random.uniform(low=0, high=1) # a parameter of membership function
+		neural_net[0][index][1].b = numpy.random.uniform(low=0, high=1) # b parameter of membership function
+		neural_net[0][index][1].antecedent_type = "B"
 
 	return neural_net
 
@@ -57,15 +66,18 @@ def membership_function(x, a=1, b=1):
 	"""
 	Calculating membership function on domain x based on 
 	function defined in homework preparations.
-	:param x: numpy.array (domain of the fuzzy set) 
+	:param x: int, float or numpy.array (domain of the fuzzy set) 
 	:param a: float
 	:param b: float
 	:return: dict
 	"""
 	member_func = 1/(1 + numpy.exp(b*(x - a)))
 	membership_dict = dict()
+	if isinstance(x, int) or isinstance(x, float):
+		return member_func
 	for element, membership in zip(x, member_func):
 		membership_dict[element] = membership
+
 	return membership_dict
 
 
@@ -123,13 +135,38 @@ def calculate_output(mean_weights, individual_rule_outputs):
 
 
 def main(number_of_rules=2):
-	samples = generate_database()
-	nn = set_net_acrhitecture(number_of_rules)
+	samples = generate_database()  # generating database of x, y, f
+	nn = set_net_acrhitecture(number_of_rules)  # getting neural net of architecture based on number of rules
+	for sample in samples:  # for every sample
+		for index, neuron_couple in enumerate(nn[0]):  # for every neuron in input neurons
+			input_neuron_A = neuron_couple[0]
+			input_neuron_B = neuron_couple[1]
+
+			# checking if neurons have the right antecedent value
+			# just in case, this can be removed later if proven good
+			if input_neuron_A.antecedent_type != "A":
+				raise ValueError("Input neuron A on index {} has wrong antecedent value!".format(index))
+			if input_neuron_B.antecedent_type != "B":
+				raise ValueError("Input neuron B on index {} has wrong antecedent value!".format(index))
+
+			# found membership values for both x and y
+			w_A = membership_function(sample.x, input_neuron_A.a, input_neuron_A.b)  # fuzzy set A is connected to x
+			w_B = membership_function(sample.y, input_neuron_B.a, input_neuron_B.b)  # fuzzy set A is connected to y
+
+			# setting outputs of input neurons to these
+			# previously calculated weights
+			nn[0][index][0].output = w_A
+			nn[0][index][1].output = w_B
 
 
+
+# TODO: make a difference between A and B part of the antecedent
+# TODO: still the same but make it better: now A and B are placed in a list together as a couple
+# but in the next layer they need to be separated so neurons will calculate them better
 if __name__ == '__main__':
-	nn = set_net_acrhitecture(2)
-	x = numpy.array([i for i in range(-4, 4)])
-	y = numpy.array([i for i in range(-4, 4)])
-	A = membership_function(x, a=2, b=3)
-	B = membership_function(y, a=1, b=1)
+	# nn = set_net_acrhitecture(2)
+	# x = numpy.array([i for i in range(-4, 4)])
+	# y = numpy.array([i for i in range(-4, 4)])
+	# A = membership_function(x, a=2, b=3)
+	# B = membership_function(y, a=1, b=1)
+	main()
